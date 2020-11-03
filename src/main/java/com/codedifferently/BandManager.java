b14f80c2dfd0e7d9bdcd51b196e4f6524675905a
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2020-11-02 21:26:42 -0500

    completed the WuBandManager[loop_labs]

--- src/main/java/com/codedifferently/BandManager.java
+++ src/main/java/com/codedifferently/BandManager.java
@@ -5,13 +5,20 @@ public class BandManager {
     private final WuMember[] wuMemberArray;
 
     public BandManager(WuMember[] wuMemberArray) {
+
         this.wuMemberArray = wuMemberArray;
     }
 
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int counter =0;
         // while `counter` is less than length of array
+        while(counter<wuMemberArray.length){
+            result +=wuMemberArray[counter];
+            counter++;
+        }
+        //while(counter)
             // begin loop
 
                 // use `counter` to identify the `current Person` in the array
@@ -27,9 +34,14 @@ public class BandManager {
     public String forLoop() {
         String result = "";
         // identify initial value
+        int counter=0;
         // identify terminal condition
         // identify increment
+for(counter=0;counter<wuMemberArray.length;counter++){
 
+result += wuMemberArray[counter];
+
+}
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -52,7 +64,9 @@ public class BandManager {
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
+        for(WuMember counter : wuMemberArray){
+              result += counter;
+        }
         return result;
     }
 
